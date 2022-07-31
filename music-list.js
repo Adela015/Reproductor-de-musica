a128001099aa80fc72412256740532b244cb2e6d
Adela015 <ade.cervantes01@gmail.com>
2022-07-31 00:30:46 -0300

    Array de canciones

---
+++ music-list.js
@@ -0,0 +1,50 @@
+let allmusic = [
+    {
+        name: "Romance",
+        artist: "SHINee",
+        img: "SHINee - Odd.jpg",
+        src: "SHINee - Romance.mp3"
+    },
+    {
+        name: "Short Hair",
+        artist: "AOA",
+        img: "AOA - Short Hair.jpg",
+        src: "AOA - Short Hair.mp3"
+    },
+    {
+        name: "Selene 6.23",
+        artist: "SHINee",
+        img: "SHINee - Selene 6.23.jpg",
+        src: "SHINee - Selene 6.23.mp3"
+    },
+    {
+        name: "I'm Home",
+        artist: "Minho",
+        img: "Minho - I'm Home.jpg",
+        src: "Minho - I'm Home.mp3"
+    },
+    {
+        name: "Imagine",
+        artist: "Key",
+        img: "Key - Face.jpg",
+        src: "Key - Face.mp3"
+    },
+    {
+        name: "Déjá-Boo",
+        artist: "JongHyun",
+        img: "Jonghyun - Base.jpg",
+        src: "Jonghyun - Base.mp3"
+    },
+    {
+        name: "Play Me",
+        artist: "Taemin",
+        img: "Taemin - Ace.jpg",
+        src: "Taemin - Ace.mp3"
+    },
+    {
+        name: "Dice",
+        artist: "Onew",
+        img: "Onew - Dice.jpg",
+        src: "Onew - Dice.mp3"
+    }
+]
